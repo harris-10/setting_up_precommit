cb92e4ceab19b62cfcf9ccfbb8784472e0d7c8da
harris-10 <ivan.harris.kouamo@gmail.com>
2024-09-23 18:26:39 +0200

    test precommit_v1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def myfunction( arg1,arg2): # Mauvais espacement et nom de fonction non conforme
-    result=arg1+arg2
-    print("The result is: ",result) # Utilisation de print au lieu de return
-    if result > 10: # Mauvaise indentation
-        print("Result is greater than 10") # Mauvaise indentation
+def myfunction(arg1, arg2):  # Mauvais espacement et nom de fonction non conforme
+    result = arg1 + arg2
+    print("The result is: ", result)  # Utilisation de print au lieu de return
+    if result > 10:  # Mauvaise indentation
+        print("Result is greater than 10")  # Mauvaise indentation
     return result
 
-myList=[1, 2, 3, 4] # Mauvais espacement
+
+myList = [1, 2, 3, 4]  # Mauvais espacement
 for i in myList:
-    print(myfunction(i, i)) # Mauvaise indentation
+    print(myfunction(i, i))  # Mauvaise indentation
